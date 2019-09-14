828ed6179d134384f2eb519d838076b2f39b0fb9
lishangjin <li.shangjin@icloud.com>
2019-09-14 19:21:41 +0800

    done.

--- common/Protocol.go
+++ common/Protocol.go
@@ -48,14 +48,14 @@ type JobExecuteResult struct {
 }
 
 type JobLog struct {
-	JobName      string `bson:"jobName"`
-	Command      string `bson:"command"`
-	Err          string `bson:"err"`
-	Output       string `bson:"output"`
-	PlanTime     int64  `bson:"planTime"`
-	ScheduleTime int64  `bson:"scheduleTime"`
-	StartTime    int64  `bson:"startTime"`
-	EndTime      int64  `bson:"endTime"`
+	JobName      string `json:"jobName" bson:"jobName"`
+	Command      string `json:"command" bson:"command"`
+	Err          string `json:"err" bson:"err"`
+	Output       string `json:"output" bson:"output"`
+	PlanTime     int64  `json:"planTime" bson:"planTime"`
+	ScheduleTime int64  `json:"scheduleTime" bson:"scheduleTime"`
+	StartTime    int64  `json:"startTime" bson:"startTime"`
+	EndTime      int64  `json:"endTime" bson:"endTime"`
 }
 
 type LogBatch struct {
